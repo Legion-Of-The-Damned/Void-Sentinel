9a7bbeac38532f15dcc585edd5154bc92c00580b
Kraken_5491 <167525281+Kraken-Legionary@users.noreply.github.com>
2025-08-31 02:28:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,20 @@
+import os
+
 def load_config():
     return {
-        "DISCORD_TOKEN": "MTIzMDQ2NDE3Njc0NTQ4MDI1NA.GfO0Zk.yCr2o3B0zEpWoDfiXaFcdSBCakVp17otLitjhA",
-        "GUILD_ID": 828749346505490503,
-
-        # Основной канал (если нужен)
-        "CHANNEL_ID": 1368377367369748480,
-
-        # Верификация
-        "VERIFY_CHANNEL_ID": 1411477389963296838,  # канал для верификации
-        "VERIFIED_ROLE_ID": 1411475618658521154,   # роль, выдаваемая после реакции
-        "VERIFY_EMOJI": "✅",  # эмодзи для реакции
-
-        "WEBHOOK_URL": "https://discord.com/api/webhooks/1375919343740784750/bFnv7e3FLprPj6eaNt6ds2KNiBzPXk-AAjVGl6noFe9GnTjoBtbeyjniDFnLEJIDRViN",
-        "IMAGE_URL": "https://cdn.discordapp.com/attachments/1355929392072753262/1370486966977691689/ChatGPT_Image_9_._2025_._22_45_29.png",
-        "AVATAR_URL": "https://cdn.discordapp.com/attachments/1355929392072753262/1367553212474982420/Void_Sentinel.jpg",
-        "CLAN_ROLE_NAMES": ["💀Легион Проклятых🔥"],
-        "CLAN_ROLE_IDS": [
-            1151988594148376709, 1347143491641151561, 1362367466390683859,
-            1248311771177947156, 1279150825998127134, 1240220163744337920
-        ],
-        "FRIEND_ROLE_ID": 1151988812151521431,
-        "GITHUB_TOKEN": "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",
-        "REPO_NAME": "Legion-Of-The-Damned/-VS-Data-Base",
-        "QUIZ_QUESTIONS_PATH": "quiz_questions.json"  # 👈 путь к файлу в репе
+        "DISCORD_TOKEN": os.getenv("DISCORD_TOKEN"),
+        "GUILD_ID": int(os.getenv("GUILD_ID", 0)),
+        "CHANNEL_ID": int(os.getenv("CHANNEL_ID", 0)),
+        "VERIFY_CHANNEL_ID": int(os.getenv("VERIFY_CHANNEL_ID", 0)),
+        "VERIFIED_ROLE_ID": int(os.getenv("VERIFIED_ROLE_ID", 0)),
+        "VERIFY_EMOJI": os.getenv("VERIFY_EMOJI", "✅"),
+        "WEBHOOK_URL": os.getenv("WEBHOOK_URL"),
+        "IMAGE_URL": os.getenv("IMAGE_URL", ""),  # можно оставить в коде, если не секрет
+        "AVATAR_URL": os.getenv("AVATAR_URL", ""),
+        "CLAN_ROLE_NAMES": os.getenv("CLAN_ROLE_NAMES", "💀Легион Проклятых🔥").split(","),
+        "CLAN_ROLE_IDS": [int(x) for x in os.getenv("CLAN_ROLE_IDS", "").split(",") if x],
+        "FRIEND_ROLE_ID": int(os.getenv("FRIEND_ROLE_ID", 0)),
+        "GITHUB_TOKEN": os.getenv("GITHUB_TOKEN"),
+        "REPO_NAME": os.getenv("REPO_NAME", "Legion-Of-The-Damned/-VS-Data-Base"),
+        "QUIZ_QUESTIONS_PATH": "quiz_questions.json"
     }
