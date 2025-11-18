5cf46412463da12733e223d7ecd049fee083eea2
Kraken_5491 <167525281+Kraken-Legionary@users.noreply.github.com>
2025-11-19 01:24:06 +0200

    Add files via upload

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,11 @@
-from .data import active_duels, save_active_duels, update_stats, load_data, stats
+from .data import (
+    stats,
+    active_duels,
+    get_stats,
+    get_active_duels,
+    update_stats,
+    save_active_duels,
+    load_data,
+    key_from_name,  # <-- добавляем сюда
+
+)
