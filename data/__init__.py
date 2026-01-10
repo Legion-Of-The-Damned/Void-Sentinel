e07b625522aa9f5062ca2e336d22b1852f614483
Kraken_5491 <167525281+Kraken-Legionary@users.noreply.github.com>
2026-01-10 02:59:36 +0200

    Полная перезапись репы

---
+++ data/__init__.py
@@ -0,0 +1,11 @@
+from .data import (
+    stats,
+    active_duels,
+    get_stats,
+    get_active_duels,
+    update_stats,
+    save_active_duels,
+    load_data,
+    key_from_name,  # <-- добавляем сюда
+
+)
